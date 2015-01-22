70207fb8ad7c92001d9854cac029eb0d297cc0e3
Pierre BLONDEAU <pierre.blondeau@unicaen.fr>
2015-01-22 19:04:32 +0100

    Update approval_notifier.rb
    Modification for compatibility with Ruby-Mailer

--- app/models/approval_notifier.rb
+++ app/models/approval_notifier.rb
@@ -6,20 +6,21 @@ class ApprovalNotifier < Mailer
 
   def waiting_for_approval(admin, project, asker)
     set_language_if_valid admin.language
-    recipients   admin.mail
-    subject      'Request for new project'
-    content_type "multipart/alternative"
-    body         :asker => asker, :project => project, :link => url_for(:controller => :admin, :action => :projects, :status => 2, :name => URI.escape(project.name))
-    render_multipart('waiting_for_approval', body)
+    @asker=asker
+    @project=project
+    @link=url_for(:controller => :admin, :action => :projects, :status => 9, :name => URI.escape(project.name))
+    mail(to: admin.mail,
+         subject: 'Request for new project',
+         template_path: 'approval_notifier',
+         template_name: 'waiting_for_approval')
   end
 
   def project_has_been_approved(user, project)
     set_language_if_valid user.language
-    recipients   user.mail
-    subject      'Accepted project notification'
-    content_type "multipart/alternative"
-    body         :project => project, :link => url_for(:controller => :projects, :action => :settings, :id => URI.escape(project.identifier))
-    render_multipart('project_has_been_approved', body)
+    @project=project
+    @link=url_for(:controller => :projects, :action => :settings, :id => URI.escape(project.identifier))
+    mail(to: user.mail,
+         subject: 'Accepted project notification')
   end
 
 end
